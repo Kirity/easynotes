e82c1fb72852989d1699c7da445797a2b9a73edb
DELL\krapu <k.rapuru@gmail.com>
2018-02-05 16:49:39 +0100

    some testing

--- src/main/java/com/myhome/easynotes/controller/NoteController.java
+++ src/main/java/com/myhome/easynotes/controller/NoteController.java
@@ -2,6 +2,7 @@ package com.myhome.easynotes.controller;
 
 
 
+import java.util.Arrays;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -9,7 +10,10 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
 import com.myhome.easynotes.model.Note;
+import com.myhome.easynotes.model.WeatherPojo;
 import com.myhome.easynotes.repository.NoteRepository;
 
 
@@ -19,6 +23,10 @@ public class NoteController {
 	
 	@Autowired
     NoteRepository noteRepository;
+	
+	RestTemplate restTemplate = new RestTemplate();
+	
+	
 
     // Get All Notes
 	@GetMapping("/notes")
@@ -30,7 +38,12 @@ public class NoteController {
 	@PostMapping("/notes")
 	public Note createNote(@Valid @RequestBody Note note) {
 		System.out.println(note);
-		return noteRepository.save(note);
+		String getUrl = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=3cfd22ebd593aafe300579b626c16438";
+		 ResponseEntity<WeatherPojo> getResponse =restTemplate.getForEntity(getUrl, WeatherPojo.class);
+		 //System.out.println("Weather in london is "+getResponse.getBody().weather);
+		 WeatherPojo w = getResponse.getBody();
+		 System.out.println("Weather in london is "+w.getMain().getTemp());
+		 return noteRepository.save(note);
 	}
 
     // Get a Single Note
@@ -45,7 +58,7 @@ public class NoteController {
 	}
 
     // Update a Note
-	// Update a Note
+	
 	@PutMapping("/notes/{id}")
 	public ResponseEntity<Note> updateNote(@PathVariable(value = "id") Long noteId, 
 	                                       @Valid @RequestBody Note noteDetails) {
@@ -55,6 +68,7 @@ public class NoteController {
 	    }
 	    note.setTitle(noteDetails.getTitle());
 	    note.setContent(noteDetails.getContent());
+	    System.out.println("which we got from ui "+noteDetails);
 
 	    Note updatedNote = noteRepository.save(note);
 	    return ResponseEntity.ok(updatedNote);
